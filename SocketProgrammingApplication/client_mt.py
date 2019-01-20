7481883c0db4fc4c62968c24185527da127969a7
ddhruvgupta <ddhruvgupta@gmail.com>
2019-01-20 18:05:39 -0500

    Add files via upload

---
+++ SocketProgrammingApplication/client_mt.py
@@ -0,0 +1,23 @@
+# @Author:Dhruv gupta
+# Class: CSC 6220
+# Homework 2
+
+import socket
+
+serverName = 'localhost'
+serverPort = 5100;
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientSocket.connect((serverName,serverPort))
+
+counter =0
+
+while (counter<10):
+    sentence = input("Enter lower case sentence: ")
+
+    clientSocket.send(sentence.encode())
+    modifiedSentence= clientSocket.recv(1024)
+    print('From server:', modifiedSentence.decode())
+    counter=counter+1
+    print('chances used(out of 10): '+ str(counter))
+
+clientSocket.close()
